1d931b05c9d6856cc4e6638a76c2363261c082ec
devsamin <abdullahalsamin79@gmail.com>
2025-09-22 01:37:52 -0700

    root layout and navber set up

---
+++ src/Shared/FastDeliveryLogo/FastDeliveryLogo.jsx
@@ -0,0 +1,12 @@
+import logo from '../../assets/banner/logo.png'
+
+const FastDeliveryLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <p className='font-semibold'>Fast Delivery</p>
+        </div>
+    );
+};
+
+export default FastDeliveryLogo;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,18 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element : <RootLayout/>,
+    children : [
+        {
+            index : true,
+            element : <Home/>
+        }
+    ]
   },
 ]);
